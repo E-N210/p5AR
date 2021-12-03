48fb615d62bbb7552e06b7210df6632eb142436a
ennio sorrentono <ennio.sor@gmail.com>
2021-12-03 15:22:20 +0100

    15

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-let chair;
 
 function setup() {
 
@@ -10,35 +9,4 @@ fill(100,240,100)
 box(20)
 translate(0,0,10)
 
-}
-
-// The process of creating the new generation
-function generate() {
-
-  // Loop through every spot in our 2D array and check spots neighbors
-  for (let x = 1; x < columns - 1; x++) {
-    for (let y = 1; y < rows - 1; y++) {
-      // Add up all the states in a 3x3 surrounding grid
-      let neighbors = 0;
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          neighbors += board[x+i][y+j];
-        }
-      }
-
-      // A little trick to subtract the current cell's state since
-      // we added it in the above loop
-      neighbors -= board[x][y];
-      // Rules of Life
-      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
-      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
-      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
-      else                                             next[x][y] = board[x][y]; // Stasis
-    }
-  }
-
-  // Swap!
-  let temp = board;
-  board = next;
-  next = temp;
 }
